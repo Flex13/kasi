577971e85109e57e141f6f516fbbbb8d98bba780
Sibanye <sibanyebukani01@gmail.com>
2020-05-20 12:32:59 +0200

    Regsiter wizerd

--- register.php
+++ register.php
@@ -42,21 +42,21 @@ $countIP = $row[0];
 
 
                     <div class="form-group">
-                        <label for="email">Username</label>
+                        <label class="form-label" for="email">Username</label>
                         <input type="text" class="form-control" id="username" value="<?php if (isset($c_username)) echo $c_username; ?>" name="Username" placeholder="Username">
                     </div>
                     <div class="form-group">
-                        <label for="email">Email</label>
+                        <label class="form-label" for="email">Email</label>
                         <input type="email" name="Email" size="32" value="<?php if (isset($email)) echo $email; ?>" maxlength="60" class="form-control" placeholder="Email Address" />
                         <small class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
                     <div class="form-group">
-                        <label>Create password</label>
+                        <label class="form-label">Create password</label>
                         <input type="password" placeholder="Enter Password" name="Password" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." />
                     </div>
                     <div class="form-group">
-                        <label>Repeat password</label>
+                        <labe class="form-label"l>Repeat password</labe>
                         <input type="password" placeholder="Confirm Password" name="Password2" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." />
                     </div>
                     <input type="button" name="next" class="next btn btn-primary-next" value="Next" />
@@ -69,12 +69,12 @@ $countIP = $row[0];
 
                     <div class="form-row">
                         <div class="col form-group">
-                            <label>First name</label>
-                            <input type="text" name="Name" size="32" maxlength="60" value="<?php if (isset($name)) echo $name; ?>" class="form-control" />
+                            <label class="form-label">First name</label>
+                            <input type="text" name="Name" size="32" maxlength="60" value="<?php if (isset($name)) echo $name; ?>" class="form-control" placeholder="Your Name" />
                         </div> <!-- form-group end.// -->
                         <div class="col form-group">
-                            <label>Last name</label>
-                            <input type="text" name="Surname" size="32" maxlength="60" value="<?php if (isset($surname)) echo $surname; ?>" class="form-control" />
+                            <label class="form-label">Last name</label>
+                            <input type="text" name="Surname" size="32" maxlength="60" value="<?php if (isset($surname)) echo $surname; ?>" class="form-control" placeholder="Your Surname" />
                         </div> <!-- form-group end.// -->
                     </div> <!-- form-row end.// -->
 
@@ -96,27 +96,27 @@ $countIP = $row[0];
                 <fieldset>
 
                     <div class="container section-heading">
-                        <h4 class="section-title text-center">Step 3: Conatct Information</h4>
+                        <h4 class="section-title text-center">Step 3: Contact Information</h4>
                     </div>
 
                     <div class="form-group">
-                        <label>Contact Details</label>
-                        <input type="tel" name="Cell" size="10" value="<?php if (isset($cell)) echo $cell; ?>" maxlength="12" class="form-control" >
+                        <label class="form-label">Contact Details</label>
+                        <input type="tel" name="Cell" size="10" value="<?php if (isset($cell)) echo $cell; ?>" maxlength="12" class="form-control" placeholder="Celpphone number" >
                     </div> <!-- form-group end.// -->
 
                     <div class="form-group">
-                        <label>Address</label>
-                        <input type="text" name="Address" size="32" value="<?php if (isset($street_address)) echo $street_address; ?>" maxlength="60" class="form-control">
+                        <label class="form-label">Address</label>
+                        <input type="text" name="Address" size="32" value="<?php if (isset($street_address)) echo $street_address; ?>" maxlength="60" class="form-control" placeholder="Street Address">
                     </div> <!-- form-group end.// -->
 
                     <div class="form-group">
-                        <label>City</label>
-                        <input type="text" name="City" size="32" value="<?php if (isset($city)) echo $city; ?>" maxlength="60" class="form-control">
+                        <label class="form-label">City</label>
+                        <input type="text" name="City" size="32" value="<?php if (isset($city)) echo $city; ?>" maxlength="60" class="form-control" placeholder="City">
                     </div> <!-- form-group end.// -->
 
 
                     <div class="form-group ">
-                        <label>Kasi</label>
+                        <label class="form-label">Kasi</label>
                         <select id="inputCity" name="Kasi" class="form-control">
                                 <option> Choose...</option>
                                 <option value="Vaal">Vaal</option>
@@ -128,7 +128,7 @@ $countIP = $row[0];
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <label>Province</label>
+                            <label class="form-label">Province</label>
                             <select id="inputProvince" name="Province" class="form-control">
                                 <option> Choose...</option>
                                 <option value="Gauteng">Gauteng</option>
@@ -144,8 +144,8 @@ $countIP = $row[0];
                         </div> <!-- form-group end.// -->
 
                         <div class="form-group col-md-6">
-                            <label>Zip</label>
-                            <input type="text" name="Zip" size="10" value="<?php if (isset($zip)) echo $zip; ?>" maxlength="10" class="form-control">
+                            <label class="form-label">Zip</label>
+                            <input type="text" name="Zip" size="10" value="<?php if (isset($zip)) echo $zip; ?>" maxlength="10" class="form-control" placeholder="zip code">
                         </div>
                     </div> <!-- form-group end.// -->
 
--- shop_register.php
+++ shop_register.php
@@ -42,39 +42,50 @@ $countIP = $row[0];
 
                     <div class="form-group">
                         <label class="form-label" for="email">Username</label>
-                        <input type="text" class="form-control" id="username" name="Username" placeholder="Username">
+                        <input type="text" class="form-control" id="username" value="<?php if (isset($m_username)) echo $m_username; ?>" name="Username" placeholder="Username">
                     </div>
                     <div class="form-group">
                         <label class="form-label" for="email">Email</label>
-                        <input type="email" name="Email" size="32" value="<?php if (isset($email)) echo $email; ?>" maxlength="60" class="form-control" placeholder="Email Address" />
+                        <input type="email" name="Email" size="32" value="<?php if (isset($email)) echo $email; ?>" maxlength="60" class="form-control" placeholder="Email address" />
                         <small class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
 
                     <div class="form-group">
                         <label class="form-label">Create password</label>
-                        <input type="password" placeholder="Enter Password" name="Password" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." />
+                        <input type="password" placeholder="Enter Password" name="Password" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." placeholder="Password" />
                     </div>
                     <div class="form-group">
                         <label class="form-label">Repeat password</label>
-                        <input type="password" placeholder="Confirm Password" name="Password2" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." />
+                        <input type="password" placeholder="Confirm Password" name="Password2" size="20" maxlength="20" id="psswd" class="input-psswd form-control" autocomplete="on" psswd-shown="false" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$" title="Please include at least 1 uppercase character, 1 lowercase character, and 1 number." placeholder="Confirm Password"/>
                     </div>
                     <input type="button" name="next" class="next btn btn-primary-next" value="Next" />
                 </fieldset>
 
                 <fieldset>
                     <div class="container section-heading">
-                        <h4 class="section-title text-center">Step 2: Shop Details</h4>
+                        <h4 class="section-title text-center">Step 2: Personal Details</h4>
                     </div>
 
+                    <div class="form-row">
+                        <div class="col form-group">
+                            <label class="form-label">First name</label>
+                            <input type="text" name="Name" size="32" maxlength="60" value="<?php if (isset($name)) echo $name; ?>" class="form-control" placeholder="Your name"/>
+                        </div> <!-- form-group end.// -->
+                        <div class="col form-group">
+                            <label class="form-label">Last name</label>
+                            <input type="text" name="Surname" size="32" maxlength="60" value="<?php if (isset($surname)) echo $surname; ?>" class="form-control" placeholder="Your surname" />
+                        </div> <!-- form-group end.// -->
+                    </div> <!-- form-row end.// -->
 
                     <div class="form-group">
-                        <label class="form-label">Shop Name</label>
-                        <input type="text" name="Name" size="32" maxlength="60" value="<?php if (isset($shop_name)) echo $shop_name; ?>" class="form-control" />
-                    </div> <!-- form-group end.// -->
-
-                    <div class="form-group">
-                        <label class="form-label">About Shop</label>
-                        <textarea class="form-control" name="Description" cols="5" rows="15"><?php if (isset($shop_description)) echo $shop_description; ?></textarea>
+                        <label class="custom-control custom-radio custom-control-inline">
+                            <input class="custom-control-input" checked="" type="radio" name="Gender" value="Male">
+                            <span class="custom-control-label"> Male </span>
+                        </label>
+                        <label class="custom-control custom-radio custom-control-inline">
+                            <input class="custom-control-input" type="radio" name="Gender" value="Female">
+                            <span class="custom-control-label"> Female </span>
+                        </label>
                     </div> <!-- form-group end.// -->
 
                     <input type="button" name="previous" class="previous btn btn-primary-previous" value="Previous" />
@@ -84,29 +95,33 @@ $countIP = $row[0];
                 <fieldset>
 
                     <div class="container section-heading">
-                        <h4 class="section-title text-center">Step 3: Conatct Information</h4>
+                        <h4 class="section-title text-center">Step 3:Personal Contact Information</h4>
                     </div>
 
                     <div class="form-group">
                         <label class="form-label">Contact Details</label>
-                        <input type="tel" name="Cell" size="10" value="<?php if (isset($cell)) echo $cell; ?>" maxlength="12" class="form-control" oninput="this.className = ''">
+                        <input type="tel" name="Cell" size="10" value="<?php if (isset($cell)) echo $cell; ?>" maxlength="12" class="form-control" placeholder="Cellphone number">
                     </div> <!-- form-group end.// -->
 
                     <div class="form-group">
                         <label class="form-label">Address</label>
-                        <input type="text" name="Address" size="32" value="<?php if (isset($address)) echo $address; ?>" maxlength="60" class="form-control" oninput="this.className = ''">
+                        <input type="text" name="Address" size="32" value="<?php if (isset($street_address)) echo $street_address; ?>" maxlength="60" class="form-control" placeholder="Street Address">
                     </div> <!-- form-group end.// -->
 
+                    <div class="form-group">
+                        <label class="form-label">City</label>
+                        <input type="text" name="City" size="32" value="<?php if (isset($city)) echo $city; ?>" maxlength="60" class="form-control" placeholder="City">
+                    </div> <!-- form-group end.// -->
 
 
                     <div class="form-group ">
                         <label class="form-label">Kasi</label>
                         <select id="inputCity" name="Kasi" class="form-control">
-                            <option> Choose...</option>
-                            <option value="Vaal">Vaal</option>
-                            <option value="Soweto">Soweto</option>
-                            <option value="Alex">Alex</option>
-                        </select>
+                                <option> Choose...</option>
+                                <option value="Vaal">Vaal</option>
+                                <option value="Soweto">Soweto</option>
+                                <option value="Alex">Alex</option>
+                            </select>
                     </div> <!-- form-group end.// -->
 
 
@@ -129,7 +144,93 @@ $countIP = $row[0];
 
                         <div class="form-group col-md-6">
                             <label class="form-label">Zip</label>
-                            <input type="text" name="Zip" size="10" value="<?php if (isset($zip)) echo $zip; ?>" maxlength="10" class="form-control" oninput="this.className = ''">
+                            <input type="text" name="Zip" size="10" value="<?php if (isset($zip)) echo $zip; ?>" maxlength="10" class="form-control" placeholder="Zip code">
+                        </div>
+                    </div> <!-- form-group end.// -->
+
+                    <input type="button" name="previous" class="previous btn btn-primary-previous" value="Previous" />
+                    <input type="button" name="next" class="next btn btn-primary-next" value="Next" />
+                </fieldset>
+
+                <fieldset>
+                    <div class="container section-heading">
+                        <h4 class="section-title text-center">Step 4: Shop Details</h4>
+                    </div>
+
+
+                    <div class="form-group">
+                        <label class="form-label">Shop Name</label>
+                        <input type="text" name="Shop_Name" size="32" maxlength="60" value="<?php if (isset($shop_name)) echo $shop_name; ?>" class="form-control" placeholder="Shop name"/>
+                    </div> <!-- form-group end.// -->
+
+                    <div class="form-group">
+                        <label class="form-label">About Shop</label>
+                        <textarea class="form-control" name="Description" cols="5" rows="15" placeholder="What type of service or products do you offer?"><?php if (isset($shop_description)) echo $shop_description; ?></textarea>
+                    </div> <!-- form-group end.// -->
+
+                    <input type="button" name="previous" class="previous btn btn-primary-previous" value="Previous" />
+                    <input type="button" name="next" class="next btn btn-primary-next" value="Next" />
+                </fieldset>
+
+                <fieldset>
+
+                    <div class="container section-heading">
+                        <h4 class="section-title text-center">Step 5:Shop Contact Information</h4>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="form-label">Email</label>
+                        <input type="Email" name="Shop_Email" size="10" value="<?php if (isset($shop_email)) echo $shop_email; ?>" maxlength="12" class="form-control" placeholder="Business email">
+                    </div> <!-- form-group end.// -->
+
+                    <div class="form-group">
+                        <label class="form-label">Contact Details</label>
+                        <input type="tel" name="Shop_Cell" size="10" value="<?php if (isset($shop_cell)) echo $shop_cell; ?>" maxlength="12" class="form-control" placeholder="Business cell">
+                    </div> <!-- form-group end.// -->
+
+                    <div class="form-group">
+                        <label class="form-label">Address</label>
+                        <input type="text" name="Shop_Address" size="32" value="<?php if (isset($shop_address)) echo $street_address; ?>" maxlength="60" class="form-control" placeholder="Business address">
+                    </div> <!-- form-group end.// -->
+
+                    <div class="form-group">
+                        <label class="form-label">City</label>
+                        <input type="text" name="Shop_City" size="32" value="<?php if (isset($shop_city)) echo $shop_city; ?>" maxlength="60" class="form-control" placeholder="Business city">
+                    </div> <!-- form-group end.// -->
+
+
+
+                    <div class="form-group ">
+                        <label class="form-label">Kasi</label>
+                        <select id="inputCity" name="Shop_Kasi" class="form-control">
+                            <option> Choose...</option>
+                            <option value="Vaal">Vaal</option>
+                            <option value="Soweto">Soweto</option>
+                            <option value="Alex">Alex</option>
+                        </select>
+                    </div> <!-- form-group end.// -->
+
+
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label class="form-label">Province</label>
+                            <select id="inputProvince" name="Shop_Province" class="form-control">
+                                <option> Choose...</option>
+                                <option value="Gauteng">Gauteng</option>
+                                <option value="Free State">Free State</option>
+                                <option value="Kwa Zulu-Natal">Kwa Zulu-Natal</option>
+                                <option value="Eastern Cape">Eastern Cape</option>
+                                <option value="Limpopo">Limpopo</option>
+                                <option value="Western Cape">Western Cape</option>
+                                <option value="Mpumalanga">Mpumalanaga</option>
+                                <option value="Northan Cape">Northan Cape</option>
+                                <option value="North West">North West</option>
+                            </select>
+                        </div> <!-- form-group end.// -->
+
+                        <div class="form-group col-md-6">
+                            <label class="form-label">Zip</label>
+                            <input type="text" name="Shop_Zip" size="10" value="<?php if (isset($shop_zip)) echo $shop_zip; ?>" maxlength="10" class="form-control" placeholder="Zip code">
                         </div>
                     </div> <!-- form-group end.// -->
 
