d712ad5cf310d74ec7c7941e12ebf87dbd40b72a
Sibanye <sibanyebukani01@gmail.com>
2020-05-28 14:18:16 +0200

    admin login page and kasi check

--- admin/login/includes/footer.php
+++ admin/login/includes/footer.php
@@ -1,8 +1,57 @@
-</div>
-  </main>
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-</body>
-</html>
+<!-- ========================= FOOTER ========================= -->
 
+
+<!-- ========================= FOOTER END // ========================= -->
+
+<!-- jQuery -->
+<script src="assets/js/jquery.js" type="text/javascript"></script>
+
+<script defer src="assets/js/all.min.js"></script>
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+<!-- custom javascript -->
+<script src="assets/js/main.js" type="text/javascript"></script>
+<script src="assets/js/bootstrap.bundle.min.js" type="text/javascript"></script>
+
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="assets/js/forms.js" type="text/javascript"></script>
+
+<script type="text/javascript">
+
+function myFunction(x) {
+  x.classList.toggle("fa");
+}
+
+</script>
+
+<script>
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+</script>
+
+<script>
+// Get the modal
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>
+</body>
+
+</html>
--- admin/login/includes/header.php
+++ admin/login/includes/header.php
@@ -1,12 +1,36 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="en">
+
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Kasi Mall Online</title>
-  <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
-  <link rel="stylesheet" href="assets/css/login.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?php if (isset($page_title)) echo $page_title; ?></title>
+
+    <!-- Bootstrap4 files-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <!-- Font awesome 5 -->
+    <link href="assets/css/all.min.css" type="text/css" rel="stylesheet">
+    <!-- Font Awesome Icon -->
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+    
+    <script src="https://kit.fontawesome.com/f9b723c806.js" crossorigin="anonymous"></script>
+
+    <!-- custom style -->
+    <link href="assets/style.css" rel="stylesheet" />
+    <link href="assets/kasi.css" rel="stylesheet" />
+    <link href="assets/ui.css" rel="stylesheet" type="text/css" />
+    <link href="assets/responsive.css" rel="stylesheet" />
+
+
+    <!--Slick-->
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
+
+
+
 </head>
+
+<div class="loader"></div>
